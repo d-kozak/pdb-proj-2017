3648bee07552b812880a9d803e2c2964ef9b4198
dkozak <dkozak94@gmail.com>
2017-12-11 22:29:10 +0100

    add timeout to select entities at task

--- src/main/java/cz/vutbr/fit/pdb/entity/concurent/geometry/SelectEntitiesAtTask.java
+++ src/main/java/cz/vutbr/fit/pdb/entity/concurent/geometry/SelectEntitiesAtTask.java
@@ -2,6 +2,7 @@ package cz.vutbr.fit.pdb.entity.concurent.geometry;
 
 import cz.vutbr.fit.pdb.db.Spatial;
 import cz.vutbr.fit.pdb.entity.geometry.Point;
+import cz.vutbr.fit.pdb.utils.JavaFXUtils;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 
@@ -10,6 +11,7 @@ public class SelectEntitiesAtTask extends Task<ObservableList<Integer>> {
 
     @Override
     protected ObservableList<Integer> call() throws Exception {
+        JavaFXUtils.startWithTimeout(3000, this);
         return Spatial.entitiesContainingPoint(point);
     }
 
