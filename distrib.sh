573c4ba4bfe509684a2123599e2ed1454cf6b8dd
dkozak <dkozak94@gmail.com>
2017-12-11 23:59:13 +0100

    add documentation and distrib

--- distrib.sh
+++ distrib.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-zip -r -9 xkozak.zip src documents body.txt build.sh clean.sh compile.sh distrib.sh init_db.sql clear_db.sql pom.xml README.md run.sh TODO.md
+rm -f xkozak15.zip
+rm -f xplask00.zip
+rm -f xvelec07.zip
+
+zip -r -9 xkozak15.zip src body.txt build.sh clean.sh compile.sh distrib.sh init_db.sql clear_db.sql pom.xml README.md run.sh TODO.md
+zip -r -9 xplask00.zip documents 
